df2e911c91de3da0acb7e239691525f50b6259a1
Jorge Migueis <jorge.migueis@valtech.co.uk>
2014-10-21 12:58:40 +0100

    US858 - twirl upgrade.

--- project/Build.scala
+++ project/Build.scala
@@ -2,13 +2,15 @@ import sbt._
 import sbt.Keys._
 
 
+
 object ApplicationBuild extends Build {
   val appName = "wsCommons"
 
-  val appVersion = "1.0"
+  val appVersion = "2.0"
 
   val appDependencies = Seq(
-    libraryDependencies += "com.typesafe.play" %% "play" % "2.2.1"
+    libraryDependencies += "com.typesafe.play" %% "play" % "2.3.5",
+    libraryDependencies += "com.typesafe.play" %% "play-ws" % "2.3.5"
   )
 
   var sR: Seq[Def.Setting[_]] = Seq(resolvers += "Carers repo" at "http://build.3cbeta.co.uk:8080/artifactory/repo/")
--- src/main/scala/utils/HttpUtils.scala
+++ src/main/scala/utils/HttpUtils.scala
@@ -1,7 +1,8 @@
 package utils
 
-import play.api.libs.ws.Response
+import play.api.libs.ws.WSResponse
 import play.api.libs.ws.WS
+import play.api.Play.current
 import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.Await
 import scala.xml.Elem
@@ -11,19 +12,19 @@ object HttpUtils {
   implicit val context = scala.concurrent.ExecutionContext.Implicits.global
 
   class HttpMethodWrapper(url:String, timeout:FiniteDuration){
-    def get[T](m:Response => T):T = Await.result(WS.url(url).get().map(m),timeout)
+    def get[T](m:WSResponse => T):T = Await.result(WS.url(url).get().map(m),timeout)
 
-    def put[T](m:Response => T) = new {
+    def put[T](m:WSResponse => T) = new {
       def exec(map:Map[String,Seq[String]] = Map.empty[String,Seq[String]]):T =  Await.result(WS.url(url).put(map).map(m),timeout)
     }
 
-    def post[T](m:Response => T) = new {
+    def post[T](m:WSResponse => T) = new {
       def exec(map:Map[String,Seq[String]] = Map.empty[String,Seq[String]]):T = Await.result(WS.url(url).post(map).map(m),timeout)
       def exec(s:String):T = Await.result(WS.url(url).post(s).map(m),timeout)
       def exec(s:Elem):T = Await.result(WS.url(url).post(s).map(m),timeout)
     }
 
-    def postXml[T](m:Response => T) = new {
+    def postXml[T](m:WSResponse => T) = new {
       def exec(xml: String): T = Await.result(WS.url(url).withHeaders("Content-Type"->"application/xml").post(xml).map(m), timeout)
     }
   }
